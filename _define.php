1da14061942b95b648464151369bbd285163f21b
franck <carnet.franck.paul@gmail.com>
2018-03-09 15:17:22 +0100

    Various code formatting

--- _define.php
+++ _define.php
@@ -1,24 +1,26 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of magnific-popup, a plugin for Dotclear 2.
-#
-# Copyright (c) Franck Paul and contributors
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief magnific-popup, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Franck Paul and contributors
+ *
+ * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-if (!defined('DC_RC_PATH')) { return; }
+if (!defined('DC_RC_PATH')) {return;}
 
 $this->registerModule(
-	/* Name */			"magnific-popup",
-	/* Description*/		"lightBox like effect on images using jquery responsive modal",
-	/* Author */			"Franck Paul and contributors",
-	/* Version */			'0.4',
-	array(
-		/* Dependencies */	'requires' =>		array(array('core','2.9')),
-		/* Permissions */	'permissions' =>	'admin',
-		/* Type */			'type' =>			'plugin'
-	)
+    "magnific-popup",                                               // Name
+    "lightBox like effect on images using jquery responsive modal", // Description
+    "Franck Paul and contributors",                                 // Author
+    '0.4',                                                          // Version
+    array(
+        'requires'    => array(array('core', '2.9')), // Dependencies
+        'permissions' => 'admin',                     // Permissions
+        'type'        => 'plugin'                    // Type
+    )
 );
--- _public.php
+++ _public.php
@@ -1,57 +1,57 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of magnific-popup, a plugin for Dotclear 2.
-#
-# Copyright (c) Franck Paul and contributors
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief magnific-popup, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Franck Paul and contributors
+ *
+ * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-if (!defined('DC_RC_PATH')) { return; }
+if (!defined('DC_RC_PATH')) {return;}
 
-$core->addBehavior('publicHeadContent',array('magnificPopupPublic','publicHeadContent'));
-$core->addBehavior('publicFooterContent',array('magnificPopupPublic','publicFooterContent'));
+$core->addBehavior('publicHeadContent', array('magnificPopupPublic', 'publicHeadContent'));
+$core->addBehavior('publicFooterContent', array('magnificPopupPublic', 'publicFooterContent'));
 
 class magnificPopupPublic
 {
-	public static function publicHeadContent($core)
-	{
-		$core->blog->settings->addNameSpace('magnificpopup');
-		if (!$core->blog->settings->magnificpopup->enabled) {
-			return;
-		}
+    public static function publicHeadContent($core)
+    {
+        $core->blog->settings->addNameSpace('magnificpopup');
+        if (!$core->blog->settings->magnificpopup->enabled) {
+            return;
+        }
 
-		echo
-		dcUtils::cssLoad($core->blog->getPF('magnific-popup/css/magnific-popup.css')).
-		dcUtils::jsLoad($core->blog->getPF('magnific-popup/js/magnific-popup.js'));
-	}
+        echo
+        dcUtils::cssLoad($core->blog->getPF('magnific-popup/css/magnific-popup.css')) .
+        dcUtils::jsLoad($core->blog->getPF('magnific-popup/js/magnific-popup.js'));
+    }
 
-	public static function publicFooterContent($core)
-	{
-		$core->blog->settings->addNameSpace('magnificpopup');
-		if (!$core->blog->settings->magnificpopup->enabled) {
-			return;
-		}
+    public static function publicFooterContent($core)
+    {
+        $core->blog->settings->addNameSpace('magnificpopup');
+        if (!$core->blog->settings->magnificpopup->enabled) {
+            return;
+        }
 
-		echo
-		'<script type="text/javascript">'."\n".
-		"//<![CDATA[\n".
-		'$(document).ready(function() {'."\n".
-			'$("div.post-content,div.post-excerpt").magnificPopup({'."\n".
-				'delegate: \'a[href$=".jpg"],a[href$=".jpeg"],a[href$=".png"],a[href$=".gif"],a[href$=".webp"],a[href$=".JPG"],a[href$=".JPEG"],a[href$=".PNG"],a[href$=".GIF"],a[href$=".WEBP"]\', '."\n".
-				'type: \'image\', '."\n".
-				'tClose: \''.__('Close (esc)').'\', '."\n".
-				'gallery: {'."\n".
-					'enabled: true,'."\n".
-					'tPrev: \''.__('Previous (Left arrow key)').'\', '."\n".
-					'tNext: \''.__('Next (Right arrow key)').'\', '."\n".
-					'tCounter: \'<span class="mfp-counter">'.__('%curr% of %total%').'</span>\', '."\n".
-				'}'."\n".
-			'});'."\n".
-		"});\n".
-		"\n//]]>\n".
-		"</script>\n";
-	}
+        echo
+        '<script type="text/javascript">' . "\n" .
+        '$(document).ready(function() {' . "\n" .
+        '$("div.post-content,div.post-excerpt").magnificPopup({' . "\n" .
+        'delegate: \'a[href$=".jpg"],a[href$=".jpeg"],a[href$=".png"],a[href$=".gif"],a[href$=".webp"],a[href$=".JPG"],a[href$=".JPEG"],a[href$=".PNG"],a[href$=".GIF"],a[href$=".WEBP"]\', ' . "\n" .
+        'type: \'image\', ' . "\n" .
+        'tClose: \'' . __('Close (esc)') . '\', ' . "\n" .
+        'gallery: {' . "\n" .
+        'enabled: true,' . "\n" .
+        'tPrev: \'' . __('Previous (Left arrow key)') . '\', ' . "\n" .
+        'tNext: \'' . __('Next (Right arrow key)') . '\', ' . "\n" .
+        'tCounter: \'<span class="mfp-counter">' . __('%curr% of %total%') . '</span>\', ' . "\n" .
+            '}' . "\n" .
+            '});' . "\n" .
+            "});\n" .
+            "</script>\n";
+    }
 }
