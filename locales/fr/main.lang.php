a1dd505dddc34e7a9ffd7ea70951ad627488319c
franck <carnet.franck.paul@gmail.com>
2023-06-12 10:18:46 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -12,7 +12,7 @@
 use Dotclear\Helper\L10n;
 
 L10n::$locales['lightBox like effect on images using jquery responsive modal'] = 'Effets lightbox-like responsive sur les images';
-L10n::$locales['magnificPopup'] = '';
+L10n::$locales['magnificPopup'] = 'magnificPopup';
 L10n::$locales['Enable magnificPopup'] = 'Activer magnificPopup';
 L10n::$locales['Enable animation'] = 'Activer les animations';
 L10n::$locales['Close (esc)'] = 'Fermer (esc)';
