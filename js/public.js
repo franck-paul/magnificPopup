5eaf008d0e416d1bfc428f58acfedf0ae28bdf9a
franck <carnet.franck.paul@gmail.com>
2023-12-16 18:08:29 +0100

    Use a specific procedure to find slide legend (figcaption from figure wrapper else image alternative text rather than link title which may contain generic "Open the media")

--- js/public.js
+++ js/public.js
@@ -5,6 +5,25 @@
 $(() => {
   const options = dotclear.getData('magnific_popup');
 
+  const findLegend = (mfp) => {
+    if (mfp.img.length) {
+      const img = mfp.img[0];
+      if (mfp.el.length) {
+        // Check if parent is figure and if yes try to find figcaption
+        const parent = mfp.el[0].parentElement;
+        if (parent.tagName === 'FIGURE') {
+          const figcaption = $(parent).find('figcaption');
+          if (figcaption.length) {
+            // figcaption found, return it's content
+            return figcaption[0].innerHTML;
+          }
+        }
+      }
+      if (img?.alt) return img.alt;
+    }
+    return '';
+  };
+
   const settings = {
     delegate: options.images,
     type: 'image',
@@ -14,6 +33,9 @@ $(() => {
       tPrev: options.previous,
       tNext: options.next,
       tCounter: `<span class="mfp-counter">${options.counter}</span>`,
+    },
+    image: {
+      titleSrc: findLegend,
     },
   };
   if (options.animate) {
