459133e4650bebc992fb064ff9467209979ffea3
franck <carnet.franck.paul@gmail.com>
2024-08-27 08:56:58 +0200

    New release 4.6

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'magnificPopup',
     'lightBox like effect on images using jquery responsive modal',
     'Franck Paul and contributors',
-    '4.5',
+    '4.6',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
